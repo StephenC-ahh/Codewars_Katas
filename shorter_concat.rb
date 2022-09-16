4356207fcce0a5e54ddf664b19c3e3d4bf1d33cf
StephenC-ahh <stephen.coatsworth@sky.com>
2022-09-16 17:43:10 +0100

    shorter concat completed

---
+++ shorter_concat.rb
@@ -0,0 +1,13 @@
+# def shorter_reverse_longer(a,b)
+#   if a.length < b.length
+#     a + b.reverse + a
+#   else
+#     b + a.reverse + b
+#   end
+# end
+
+# refactor
+
+def shorter_reverse_longer(a,b)
+  a.length < b.length ?  a+b.reverse+a : b+a.reverse+b
+end
